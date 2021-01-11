16cc4cd25922397950ab72534aaf068709b86d61
E-Burton <edwina.antoinette@gmail.com>
2021-01-10 21:26:34 -0700

    Updated schema.sql and added course curriculum seed data in seed.sql file for Business Analytics major.

--- app/models/program-course.js
+++ app/models/program-course.js
@@ -1,12 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
     const Course = sequelize.define('Course', {
       name: DataTypes.STRING,
-      degreeProgram: DataTypes.STRING,
+      degree_program: DataTypes.STRING,
       type: DataTypes.STRING,
-      credits: DataTypes.INTEGER,
-      preReq: DataTypes.STRING,
-      status: DataTypes.STRING,
-      letterGrade: DataTypes.STRING,
+      credits: DataTypes.STRING,
+      pre_req: DataTypes.STRING,
+      status: {type: DataTypes.STRING, defaultValue: "Not Complete"},
+      letter_grade: {type: DataTypes.STRING, defaultValue: null},
+      credits_received: {type: DataTypes.INTEGER, defaultValue: null},
+      notes: {type: DataTypes.STRING, defaultValue: null},
+      createdAt: {type: DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
+      updatedAt: {type: DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
     });
     return Course;
   };
