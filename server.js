d8f273865e04afc0cec67550a5c5a11106ebb159
Evan Gerweck <gerweckevan@gmail.com>
2021-01-12 19:08:51 -0700

    html template set up

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-
+//import express, { urlencoded, json, static } from "express";
+const express = require("express");
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3306;
 
 // Requiring our models for syncing
-const db = require('./app/models');
+const db = require("./app/models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Routes
-// require('./app/routes/api-routes.js')(app);
+// require("./app/routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(() => {
