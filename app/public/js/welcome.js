b48052ef4916e340acdb220acda0c4af6b05da60
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-01-14 20:07:31 -0600

    edited folder structure

---
+++ app/public/js/welcome.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
+
--- server.js
+++ server.js
@@ -2,22 +2,30 @@
 const express = require("express");
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 
+
+// const htmlRoutes = require("./app/routes/html");
 // Requiring our models for syncing
 const db = require("./app/models");
+const exphbs = require('express-handlebars'); 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("app/public"));
+// app.use("/", htmlRoutes);
 
 // Routes
-// require("./app/routes/api-routes.js")(app);
+require("./app/routes/html.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+db.sequelize.sync({force: true}).then(() => {
+ 
 });
+ app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
